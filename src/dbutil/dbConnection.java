ef8f662caaf9d71d1ad3ae9845a16079a9626701
game1995 <game_130@hotmail.com>
2018-02-14 11:44:04 +0700

    loginModel

--- src/dbutil/dbConnection.java
+++ src/dbutil/dbConnection.java
@@ -1,22 +1,18 @@
 package dbutil;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCON = "jdbc:school.sqlite";
-
-    public static Connection getConnection(){
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
+    public static Connection getConnection () throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(SQCON);
-        }catch (ClassNotFoundException ex){
-                ex.printStacktrace();
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException ex){
+            ex.printStackTrace();
         }
-         return null;
-        }
-
-
+        return null;
     }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
-public class Controller {
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private loginModel model = new loginModel();
+    //import object from scence builder
+    @FXML
+    private Label dbStatus;
+    @FXML
+    private Label loginStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.model.inDatabaseConnection()){
+            this.dbStatus.setText("Connected to DB");
+        }else{
+            this.dbStatus.setText("Not Connect to DB");
+        }
+
+    }//intitial
+    @FXML
+    public void Login(ActionEvent event){
+        System.out.println(username.getText());
+        System.out.println(password.getText());
+
+        try {
+            if (this.model.isLogin(username.getText(),password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+                loginStatus.setText("Welcone to Our System");
+            }else {
+                loginStatus.setText("Your username or password is invalid.");
+
+            }
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }//Login
+}//class
